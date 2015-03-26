6db3a0ad7bc617598a19261f0c745ac6a2da31d5
FCald <fabienne_calderara@hotmail.com>
2015-03-26 21:23:34 +0100

    Chume ned drus...

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Nicht mehr in Solothurn.");
-		System.out.println("Änderung.");
+		// System.out.println("Nicht mehr in Solothurn.");
+		// System.out.println("Änderung.");
+
+		System.out
+				.println("Rächt unöbersichtlich do inne...? (das änglisch esch es biz chinesisch -.- ) \n"
+						+ "Wa muessi mache, wenni en Kopie vo minere BMI Klass wett ufelade...?\n"
+						+ "Und i wetts gern unter \"ExercicesBFH\" ablegge.");
 	}
 
 }
